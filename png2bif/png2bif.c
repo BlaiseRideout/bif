7d03b11fda791ad472d0ebdf1ba015aa64b24846
Blaise Ritchie <rippinblaise@gmail.com>
2011-11-04 21:57:07 -0700

    That last commit was terrible, this one has no errors, but still unfinished

--- png2bif/png2bif.c
+++ png2bif/png2bif.c
@@ -39,11 +39,9 @@ static void     errout(int status, char *str);
 static void     cleanup(void);
 static void     handle_args(int argc, char **argv);
 static void     init(void);
-static char     *read_word(FILE *fp);
-static pixel_t  *get_color(bifcolor *colors, char *name);
 static void     write_bif(bif *bifstruct, FILE *biff);
 static void     bitmap_to_bif(bif *bifstruct, bitmap_t *bitmap);
-static int      get_png(bitmap_t *bitmap, FILE *png);
+static void     read_png(bitmap_t *bitmap, FILE *png);
 
 static void errout(int status, char *str) {
   if(str)
@@ -67,18 +65,18 @@ static void handle_args(int argc, char **argv) {
   if(argc == 4)
     verbose = 1;
 
-  if(!(biffile = fopen(argv[1], "r")))
-    errout(1, "Cannot open source bif file");
-  if((pngfile = fopen(argv[2], "r"))) {
+  if(!(pngfile = fopen(argv[1], "r")))
+    errout(1, "Cannot open source png file");
+  if((biffile = fopen(argv[2], "r"))) {
     printf("Warning: this will overwrite %s. Continue(y/n)? ", argv[2]);
     c = getchar();
     if(c != 'y' && c != 'Y')
       exit(0);
    }
-   if(pngfile)
-     fclose(pngfile);
-   if(!(pngfile = fopen(argv[2], "w")))
-     errout(1, "Cannot open png file for writing");
+   if(biffile)
+     fclose(biffile);
+   if(!(biffile = fopen(argv[2], "w")))
+     errout(1, "Cannot open bif file for writing");
 }
 
 static void init(void) {
@@ -90,142 +88,116 @@ static void init(void) {
     errout(-1, "Error allocating bif structure");
 }
 
-static char *read_word(FILE *fp) {
-  char *word;
-  char c;
-  unsigned int length = 1;
-  unsigned int i;
-
-  word = malloc(length * sizeof(char));
-
-  while((c = fgetc(fp)) != EOF)
-    if(c != ' ' && c != '\n' && c != '\t')
-      break;
-  word[length - 1] = c;
-
-  for(i = 0; (c = fgetc(fp)) != EOF; i++) {
-    if(c != ' ' && c != '\n' && c != '\t') {
-      word = realloc(word, ++length * sizeof(char));
-      word[length - 1] = c;
-    }
-    else
-      break;
-  }
-  if(c == EOF) {
-    word[length - 1] = c;
-  }
-  word = realloc(word, ++length * sizeof(char));
-  word[length - 1] = '\0';
-  return word;
-}
-
-static pixel_t *get_color(bifcolor *colors, char *name) {
-  unsigned int i;
-  for(i = 0; ; ++i) {
-    if(!strcmp(colors[i].name, name))
-      return &colors[i].color;
-  }
-}
-
 static void write_bif(bif *bifstruct, FILE *biff) {
   if(!(bifstruct && biff))
     errout(-1, "bif file not accessible or bif struct not allocated");
   unsigned int x, y;
 
   fprintf(biff, "%d %d\n[colors]\n", bifstruct->width, bifstruct->height);
-  for(x = 0; x < bitmap->numcolors; ++x) {
-    fprintf("%s %d %d %d %d\n", bifstruct->colors[i].name, bifstruct->colors[i].color.red,
-                              bifstruct->colors[i].color.green, bifstruct->colors[i].color.blue,
-                              bifstruct->colors[i].color.alpha);
+  for(x = 0; x < bifstruct->numcolors; ++x) {
+    fprintf(biff, "%s %d %d %d %d\n", bifstruct->colors[x].name, bifstruct->colors[x].color.red,
+                              bifstruct->colors[x].color.green, bifstruct->colors[x].color.blue,
+                              bifstruct->colors[x].color.alpha);
   }
   fprintf(biff, "[/colors]\n[data]\n");
 
-  for(y = 0; y < bitmap->height; ++y) {
-    for(x = 0; x < bitmap->width; ++x) {
-      fprintf(biff, "%s ", bitmap->data[y * bitmap->width + x]);
+  for(y = 0; y < bifstruct->height; ++y) {
+    for(x = 0; x < bifstruct->width; ++x) {
+      fprintf(biff, "%s ", bifstruct->data[y * bifstruct->width + x]);
     }
     fprintf(biff, "\n");
   }
 
-  fprintf(biff, "[/data]");
+  fprintf(biff, "[/data]\n");
 }
 
-static void bif_to_bitmap(bif *bifstruct, bitmap_t *bitmap) {
+static void bitmap_to_bif(bif *bifstruct, bitmap_t *bitmap) {
   if(!(bitmap && bifstruct))
     errout(-1, "No bif data to convert from or image data uninitialized");
-  unsigned int x, y;
   unsigned int i;
 
   bifstruct->width = bitmap->width;
   bifstruct->height = bitmap->height;
 
-  bifstruct->data = malloc(sizeof(char *) * width * height);
-  for(i = 0; i < width * height; ++i) {
+  bifstruct->data = malloc(sizeof(char *) * bifstruct->width * bifstruct->height);
+  for(i = 0; i < bifstruct->width * bifstruct->height; ++i) {
+    pixel_t *pixel = &bitmap->pixels[i];
     bifstruct->data[i] = calloc(sizeof(char), 256);
+//    printf("Red: %d  Green: %d Blue: %d Alpha: %d\n", pixel->red, pixel->green, pixel->blue, pixel->alpha);
   }
-
 }
 
 static void read_png(bitmap_t *bitmap, FILE *png) {
   if(!(bitmap && png))
     errout(-1, "Error reading png");
 
-  char header[8];
+  png_byte header[8];
+  int width, height;
+  png_byte color_type;
+  png_byte bit_depth;
+
   png_structp png_ptr;
-  png_infop info_ptr, end_info;
+  png_infop info_ptr;
   png_bytep *row_pointers;
-  unsigned int height;
-  unsigned int width;
-  unsigned short depth;
-  unsigned int color_type;
-  unsigned int filter_method;
-  unsigned int compression_type;
-  unsigned int interlace_type;
+
+  unsigned int y, x;
 
   fread(header, 1, 8, png);
-  if(png_sig_cmp(header, 0, 8)
-    errout(-1, "File not png");
+  if(png_sig_cmp(header, 0, 8))
+    errout(-1, "File not a png file");
 
-  png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
-                                   NULL, NULL, NULL);
+  png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
   if(!png_ptr)
-    errout(-1, "Error reading png");
+    errout(-1, "png_create_read_struct failed");
+
   info_ptr = png_create_info_struct(png_ptr);
   if(!info_ptr)
-    errout(-1, "Error reading png info");
-  end_info = png_create_info_struct(png_ptr)
-  if(!end_info)
-    errout(-1, "Error reading png info");
+    errout(-1, "png_create_info_struct failed");
 
   if(setjmp(png_jmpbuf(png_ptr)))
-    errout(-1, "Error initializing longjmp");
+    errout(-1, "error initializing png");
 
   png_init_io(png_ptr, png);
-
   png_set_sig_bytes(png_ptr, 8);
 
   png_read_info(png_ptr, info_ptr);
-  png_get_IHDR(png, ptr, info_ptr, &width, &height,
-               &depth, &color_type, &interlace_type
-               &compression_type, &filter_method);
-  if(interlace_type == PNG_INTERLACE_ADAM7)
-    errout(-1, "png file passed uses adam7 interlacing");
 
-  if(color_type = PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_GRAY)
+  width = png_get_image_width(png_ptr, info_ptr);
+  height = png_get_image_height(png_ptr, info_ptr);
+  color_type = png_get_color_type(png_ptr, info_ptr);
+  bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+  png_read_update_info(png_ptr, info_ptr);
+
+  if(setjmp(png_jmpbuf(png_ptr)))
+    errout(-1, "Error reading png");
+
+  if(color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_GRAY)
     png_set_add_alpha(png_ptr, 255, PNG_FILLER_AFTER);
-  if(color_type = PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+  if(color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
     png_set_gray_to_rgb(png_ptr);
 
-  bitmap->width = width;
-  bitmap->height = height;
-
   row_pointers = calloc(sizeof(png_bytep), height);
-  row_pointers = png_get_rows(png_ptr, info_ptr);
+  for(y = 0; y < height; y++)
+    row_pointers[y] = calloc(sizeof(png_byte), width);
 
   png_read_image(png_ptr, row_pointers);
 
-  png_read_end(png_ptr, end_info);
-  png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+  bitmap->width = width;
+  bitmap->height = height;
+  bitmap->pixels = calloc(sizeof(pixel_t), width * height);
+
+  for(y = 0; y < height; y++) {
+    png_bytep row;
+    row = row_pointers[y];
+    for(x = 0; x < width; x++) {
+      pixel_t *pixel = &bitmap->pixels[bitmap->width * y + x];
+      pixel->red = *row++;
+      pixel->green = *row++;
+      pixel->blue = *row++;
+      pixel->alpha = *row++;
+    }
+  }
 }
 
 int main(int argc, char **argv) {
@@ -236,23 +208,22 @@ int main(int argc, char **argv) {
   }
   init();
   if(verbose) {
-    puts("Parsing bif...");
+    puts("Parsing png...");
     fflush(stdout);
   }
-  parse_bif(gbif, biffile);
+  read_png(image, pngfile);
   if(verbose) {
-    puts("bif parsed\nConverting to png...");
+    puts("png parsed\nConverting to bif...");
     fflush(stdout);
   }
-  bif_to_bitmap(gbif, image);
+  bitmap_to_bif(gbif, image);
   if(verbose) {
-    puts("Converted to png\nWriting to file...");
+    puts("Converted to bif\nWriting to file...");
     fflush(stdout);
   }
-  if(write_png(image, pngfile))
-    errout(-1, "Problem writing png file");
+  write_bif(gbif, biffile);
   if(verbose) {
-    puts("png written");
+    puts("bif written");
     fflush(stdout);
   }
   cleanup();
